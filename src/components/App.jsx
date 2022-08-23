3628f12c1ff9e6c67513c4e7bc33a58ede2a98e9
pavlopetr <rezar66612@gmail.com>
2022-08-23 18:51:30 +0300

    header+

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import HomePage from 'pages/HomePage/HomePage';
 import MainPage from 'pages/MainPage/MainPage';
-
+import Header from "./Header/Header"
 export const App = () => {
   return (
 
     <div>
+       <Header />
       <MainPage />
 
 
