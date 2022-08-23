38ac185bd409cf0f876644a074fe3f00b7b489e9
pavlopetr <rezar66612@gmail.com>
2022-08-23 16:02:15 +0300

    add font

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,9 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
+        fontWeight: 700
+        
       }}
     >
       React homework template
