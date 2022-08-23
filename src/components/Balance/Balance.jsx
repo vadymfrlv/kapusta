2b8344218700b27ca7b3ae666d6661379f08151b
SerhiyDukh1984 <s.dux2011@ukr.net>
2022-08-23 17:37:11 +0300

    add start files

---
+++ src/components/Balance/Balance.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+// import { useDispatch } from 'react-redux';
+import s from './Balance.module.css';
+
+export const Balance = () => {
+  //   const dispatch = useDispatch();
+  const { input, setInput } = useState(0);
+
+  const handleChange = e => {
+    const { value } = e.target;
+
+    setInput(value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    // dispatch(addContact({ name: name, number: number }));
+  };
+
+  return (
+    <form className={s.balance} onSubmit={handleSubmit}>
+      <h3 className={s.title}>Balance:</h3>
+      <label className={s.label}>
+        <input className={s.input} value={input} onChange={handleChange} />
+      </label>
+      <button className={s.button} type="submit">
+        CONFIRM
+      </button>
+    </form>
+  );
+};
