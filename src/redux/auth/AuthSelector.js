bbb3daa2d11d373598bf3c7b0a76ce6677b9997a
SerhiyDukh1984 <s.dux2011@ukr.net>
2022-08-25 10:48:56 +0300

    add selectors

--- src/redux/auth/AuthSelector.js
+++ src/redux/auth/AuthSelector.js
@@ -3,5 +3,5 @@ export const getIsAuth = state => Boolean(state.auth.token);
 export const getMustCurUser = state =>
   state.auth.token && !state.auth.user.email;
 
-export const getMustContacts = state =>
-  state.auth.user.email && !state.contacts.items;
+// export const getMustContacts = state =>
+//   state.auth.user.email && !state.contacts.items;
