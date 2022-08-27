8a2ab50eb4f88c0c8ccef6ece10378a6928adb0a
GubarievaAnna <annagubarieva@gmail.com>
2022-08-27 12:58:56 +0300

    update

--- src/components/LogoHeader/LogoHeader.jsx
+++
@@ -1,6 +0,0 @@
-import styles from './LogoHeader.module.css';
-
-  const logoHeader = () => {
-  return <span className={styles.link} aria-label="Логотип сайту"></span>;
-}
-export default logoHeader
