f3d1a73dbe2e54eb0bfdf17ead44a7c224c96684
wanyyf <vanyaposta@gmail.com>
2022-08-28 21:31:53 +0300

    all

--- src/components/SharedLayout/SharedLayout .jsx
+++ src/components/SharedLayout/SharedLayout .jsx
@@ -2,12 +2,11 @@ import { Suspense } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 
 import Header from 'components/Header/Header';
-import UserMenuHeader from 'components/UserMenuHeader/UserMenuHeader';
-import { getIsAuth } from 'redux/auth/AuthSelector';
+
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-import s from './sharedLayout.module.css';
+import s from './SharedLayout.module.css';
 
 const SharedLayout = () => {
   const { pathname } = useLocation();
@@ -16,17 +15,19 @@ const SharedLayout = () => {
     <>
       <Header />
       <section className={pathname === '/' ? s.section : s.sectionHome}>
-        <div className={s.container}>
-          <div className={s.background}>
-            {pathname === '/' && (
-              <div className={s.bottomSection}>
-                <h1 className={s.title}>Kapusta</h1>
-                <p className={s.tittleText}>Smart Finance</p>
-              </div>
-            )}
-            <Suspense fallback={<div>Loading page...</div>}>
-              <Outlet />
-            </Suspense>
+        <div className={s.BottomHomeCabagge}>
+          <div className={s.container}>
+            <div className={pathname === '/' ? s.background : s.backgroundHome}>
+              {pathname === '/' && (
+                <div className={s.bottomSection}>
+                  <h1 className={s.title}>Kapusta</h1>
+                  <p className={s.tittleText}>Smart Finance</p>
+                </div>
+              )}
+              <Suspense fallback={<div>Loading page...</div>}>
+                <Outlet />
+              </Suspense>
+            </div>
           </div>
         </div>
       </section>
