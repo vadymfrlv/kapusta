411d0c20cc273739749336437cb9902eccc04415
wanyyf <vanyaposta@gmail.com>
2022-08-28 19:54:20 +0300

    all

--- src/components/SharedLayout/SharedLayout .jsx
+++ src/components/SharedLayout/SharedLayout .jsx
@@ -2,7 +2,6 @@ import { Suspense } from 'react';
 import { useSelector } from 'react-redux';
 import { Outlet, useLocation } from 'react-router-dom';
 import Header from 'components/Header/Header';
-import Loader from 'components/Loader/Loader';
 import UserMenuHeader from 'components/UserMenuHeader/UserMenuHeader';
 import { getIsAuth } from 'redux/auth/AuthSelector';
 import { ToastContainer } from 'react-toastify';
@@ -18,15 +17,19 @@ const SharedLayout = () => {
     <>
       {isLoggedIn ? <UserMenuHeader /> : <Header />}
       <section className={pathname === '/' ? s.section : s.sectionHome}>
-        {pathname === '/' && (
-          <div className={s.bottomSection}>
-            <h1 className={s.title}>Kapusta</h1>
-            <p className={s.tittleText}>Smart Finance</p>
+        <div className={s.container}>
+          <div className={s.background}>
+            {pathname === '/' && (
+              <div className={s.bottomSection}>
+                <h1 className={s.title}>Kapusta</h1>
+                <p className={s.tittleText}>Smart Finance</p>
+              </div>
+            )}
+            <Suspense fallback={<div>Loading page...</div>}>
+              <Outlet />
+            </Suspense>
           </div>
-        )}
-        <Suspense fallback={<Loader />}>
-          <Outlet />
-        </Suspense>
+        </div>
       </section>
       <ToastContainer />
     </>
