3b17532edd72135da3792234f081951966f78475
SerhiyDukh1984 <s.dux2011@ukr.net>
2022-08-28 15:07:45 +0300

    Update Summary.jsx

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -3,8 +3,25 @@ import s from './Summary.module.css';
 
 export const Summary = () => {
   const stats = useSelector(state => state.monthsStats.items);
-  const monthsOfYear = Object.entries(stats);
-  const filteredMonths = monthsOfYear.filter(month => month[1] !== 'N/A');
+  const keys = Object.keys(stats);
+  const values = Object.values(stats);
+
+  const monthEn = [
+    [(keys[0] = 'January'), values[0]],
+    [(keys[1] = 'February'), values[1]],
+    [(keys[2] = 'March'), values[2]],
+    [(keys[3] = 'April'), values[3]],
+    [(keys[4] = 'May'), values[4]],
+    [(keys[5] = 'June'), values[5]],
+    [(keys[6] = 'July'), values[6]],
+    [(keys[7] = 'August'), values[7]],
+    [(keys[8] = 'September'), values[8]],
+    [(keys[9] = 'October'), values[9]],
+    [(keys[10] = 'November'), values[10]],
+    [(keys[11] = 'Desember'), values[11]],
+  ];
+
+  const filteredMonths = monthEn.filter(month => month[1] !== 'N/A');
 
   return (
     <div className={s.summary}>
