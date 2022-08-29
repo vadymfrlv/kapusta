379eb0b892a99d46a4c79cbecb999fb9fbf40d41
GubarievaAnna <annagubarieva@gmail.com>
2022-08-29 17:34:44 +0300

    update

--- src/components/TransactionList/TransactionList.jsx
+++ src/components/TransactionList/TransactionList.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import expenses from '../../data/expensesReports.json';
 import { removeTransaction } from 'redux/transaction/transaction-operations';
-import { getEmailUser } from 'redux/auth/AuthSelector';
 import s from './TransactionList.module.css';
+import { useDispatch } from 'react-redux';
 
-const TransactionList = ({ operation, selector, location, date }) => {
-  const transactions = useSelector(selector);
+const TransactionList = ({ transactionsArray, location }) => {
   const dispatch = useDispatch();
-  const email = useSelector(getEmailUser);
-
-  useEffect(() => {
-    if (email) dispatch(operation);
-    // eslint-disable-next-line
-  }, [transactions.length, email]);
-
   return (
     <table className={s.table}>
       <thead>
@@ -29,44 +19,39 @@ const TransactionList = ({ operation, selector, location, date }) => {
         </tr>
       </thead>
       <tbody className={s.tableBody}>
-        {transactions &&
-          transactions
-            .filter(el => el.date === date.toISOString().slice(0, 10))
-            .map(item => (
-              <tr key={item._id}>
-                <td>{item.date.split('-').reverse().join('.')}</td>
-                <td>{item.description}</td>
-                <td>
-                  {location === 'expenses'
-                    ? expenses[item.category].title
-                    : item.category}
-                </td>
-                <td
-                  className={location === 'expenses' ? s.expenses : s.incomes}
-                >
-                  {location === 'expenses' && '-'}
-                  &nbsp;
-                  {item.amount
-                    .toFixed(2)
-                    .replace(/(\d)(?=(\d{3})+([^\d]|$))/g, '$1 ')}
-                  &nbsp; грн
-                </td>
-                <td>
-                  <button
-                    className={s.buttonDelete}
-                    onClick={() => dispatch(removeTransaction(item._id))}
-                  ></button>
-                </td>
-              </tr>
-            ))}
+        {transactionsArray &&
+          transactionsArray.map(item => (
+            <tr key={item._id}>
+              <td>{item.date.split('-').reverse().join('.')}</td>
+              <td>{item.description}</td>
+              <td>
+                {location === 'expenses'
+                  ? expenses[item.category].title
+                  : item.category}
+              </td>
+              <td className={location === 'expenses' ? s.expenses : s.incomes}>
+                {location === 'expenses' && '-'}
+                &nbsp;
+                {item.amount
+                  .toFixed(2)
+                  .replace(/(\d)(?=(\d{3})+([^\d]|$))/g, '$1 ')}
+                &nbsp; грн
+              </td>
+              <td>
+                <button
+                  className={s.buttonDelete}
+                  onClick={() => dispatch(removeTransaction(item._id))}
+                ></button>
+              </td>
+            </tr>
+          ))}
       </tbody>
     </table>
   );
 };
 
 TransactionList.propTypes = {
-  operation: PropTypes.func.isRequired,
-  selector: PropTypes.func.isRequired,
+  transactionsArray: PropTypes.array.isRequired,
   location: PropTypes.string.isRequired,
 };
 
