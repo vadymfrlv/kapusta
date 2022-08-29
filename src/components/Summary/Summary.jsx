2e5ec9bce7f0eff69e4c7ab49e59d6db06238d5a
GubarievaAnna <annagubarieva@gmail.com>
2022-08-29 19:39:08 +0300

    update

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -4,7 +4,6 @@ import month from '../../data/months';
 import s from './Summary.module.css';
 import { useTranslation } from 'react-i18next';
 
-
 export const Summary = ({ monthStats }) => {
   const currentDate = useRef(new Date().getMonth());
   const array = Object.entries(monthStats);
@@ -12,23 +11,30 @@ export const Summary = ({ monthStats }) => {
     (el, index) => index <= currentDate.current
   );
 
-  const filteredMonths = array.filter((el, index) => index <= currentDate.current);
+  const filteredMonths = array.filter(
+    (el, index) => index <= currentDate.current
+  );
   const { t } = useTranslation();
 
   return (
     <div className={s.summary}>
-    {filteredMonthsStats.length > 0 && (
-      <ul className={s.list}>
-        <li className={s.title}>{t('summary.summary')}</li>
-        {filteredMonths.map(el => (
-          <li key={el[0]} className={s.item}>
-            <p>{month[el[0]]}</p>
-            <p>
-              {el[1] === 'N/A' ? 0 : el[1].toFixed(2).replace(/(\d)(?=(\d{3})+([^\d]|$))/g, '$1 ')}
-            </p>
-          </li>
-        ))}
-      </ul>}
+      {filteredMonthsStats.length > 0 && (
+        <ul className={s.list}>
+          <li className={s.title}>{t('summary.summary')}</li>
+          {filteredMonths.map(el => (
+            <li key={el[0]} className={s.item}>
+              <p>{month[el[0]]}</p>
+              <p>
+                {el[1] === 'N/A'
+                  ? 0
+                  : el[1]
+                      .toFixed(2)
+                      .replace(/(\d)(?=(\d{3})+([^\d]|$))/g, '$1 ')}
+              </p>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
