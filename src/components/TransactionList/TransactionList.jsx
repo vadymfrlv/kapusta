5976f3d692d77317687a66759a073279d7ff061d
GubarievaAnna <annagubarieva@gmail.com>
2022-08-29 21:53:44 +0300

    Update TransactionList.jsx

--- src/components/TransactionList/TransactionList.jsx
+++ src/components/TransactionList/TransactionList.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { useTranslation } from 'react-i18next';
+import { nanoid } from 'nanoid';
 // import expenses from '../../data/expensesReports.json';
 import { removeTransaction } from 'redux/transaction/transaction-operations';
 import s from './TransactionList.module.css';
-import { useDispatch } from 'react-redux';
-import { useTranslation } from 'react-i18next';
 
 const TransactionList = ({ transactionsArray, location }) => {
   const dispatch = useDispatch();
 
   const { t } = useTranslation();
   const expenseReport = t('expenseReport', { returnObjects: true });
+
+  const createNewArray = array => {
+    const addArray = Array(10 - array.length)
+      .fill({
+        _id: '',
+        description: '',
+        amount: null,
+        date: '',
+        category: '',
+      })
+      .map(el => ({ ...el, _id: nanoid() }));
+    return [...array, ...addArray];
+  };
+
+  const arrayTable =
+    transactionsArray.length < 10
+      ? createNewArray(transactionsArray)
+      : transactionsArray;
 
   return (
     <table className={s.table}>
@@ -24,26 +43,35 @@ const TransactionList = ({ transactionsArray, location }) => {
         </tr>
       </thead>
       <tbody className={s.tableBody}>
-        {transactionsArray &&
-          transactionsArray.map((item, index) => (
-            <tr key={item._id}>
-              <td>{item.date.split('-').reverse().join('.')}</td>
-              <td>{item.description}</td>
-              <td>{location === 'expenses' ? expenseReport[index].title : item.category}</td>
-              <td className={location === 'expenses' ? s.expenses : s.incomes}>
-                {location === 'expenses' && '-'}
-                &nbsp;
-                {item.amount.toFixed(2).replace(/(\d)(?=(\d{3})+([^\d]|$))/g, '$1 ')}
-                &nbsp; {t('general.currencyName')}
-              </td>
-              <td>
+        {arrayTable.map((item, index) => (
+          <tr key={item._id}>
+            <td>{item.date && item.date.split('-').reverse().join('.')}</td>
+            <td>{item.description && item.description}</td>
+            <td>
+              {item.category && location === 'expenses'
+                ? expenseReport[index].title
+                : item.category}
+            </td>
+            <td className={location === 'expenses' ? s.expenses : s.incomes}>
+              {location === 'expenses' && item.amount && '-'}
+              &nbsp;
+              {item.amount &&
+                `${item.amount
+                  .toFixed(2)
+                  .replace(/(\d)(?=(\d{3})+([^\d]|$))/g, '$1 ')} ${t(
+                  'general.currencyName'
+                )}`}
+            </td>
+            <td>
+              {item.amount && (
                 <button
                   className={s.buttonDelete}
                   onClick={() => dispatch(removeTransaction(item._id))}
                 ></button>
-              </td>
-            </tr>
-          ))}
+              )}
+            </td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
